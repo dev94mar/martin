df2f5a7ba7bba4a121c35250f864d9f48b759536
dev94mar <dev94mar@outlook.com>
2025-02-26 20:36:07 -0600

    4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,9 +13,8 @@ release = '1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-extensions = [
-    'sphinx.ext.duration',
-]
+
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -25,5 +24,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
+html_theme = 'alabaster'
 html_static_path = ['_static']
